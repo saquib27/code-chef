6d1b6430665241bde9890e8e503af91d16baadf1
saquib27 <saquibshamshi27@gmail.com>
2026-01-22 11:42:28 +0530

    array colouring

---
+++ arraycolour.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+        }
+
+        int possible = 1;
+
+        for (int i = 0; i < n - 1; i++) {
+            if ((a[i] % 2) == (a[i + 1] % 2)) {
+                possible = 0;
+                break;
+            }
+        }
+
+        if (possible)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+}
